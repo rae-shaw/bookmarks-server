8f9f7cd048542c476fec53dbbbb2d286470a99f2
Rachael Shaw <rachaelleeshaw@gmail.com>
2019-08-23 15:04:26 -0500

    finished server

---
+++ src/app.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const logger = require('./logger')
+const bookmarksRouter = require('./bookmarks-router/bookmarks-router')
+
+const app = express()
+
+const morganOption = (NODE_ENV === 'production')
+ 	? 'tiny'
+ 	: 'common';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+
+function validateBearerToken(req, res, next) {
+  const authToken = req.get('Authorization')
+  logger.error(`Unauthorized request to path: ${req.path}`)
+
+  if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+
+  next()
+}
+
+app.use(bookmarksRouter)
+
+app.get('/', (req, res) => {
+	res.send('Hello, world!')
+})
+
+app.use(function errorHandler(error, req, res, next) {
+	let response
+	if (NODE_ENV === 'production') {
+		response = { error: { message: 'server error' } }
+	} else {
+		console.error(error)
+		response = { message: error.message, error }
+	}
+	res.status(500).json(response)
+})
+
+module.exports = app
